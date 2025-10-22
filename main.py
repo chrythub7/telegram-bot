86605141b17f2ffa0452ab489491fcf833cece34
chrythub7 <132924147+chrythub7@users.noreply.github.com>
2025-10-22 16:42:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from telebot import TeleBot
 
-bot = TeleBot("INSERISCI_IL_TUO_TOKEN")  # <-- metti qui il token di BotFather
+bot = TeleBot("8075827806:AAFLwKd9_jJ2s39eGK_64gs2X3CWJPlwwso")  # <-- metti qui il token di BotFather
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "👋 Ciao! Il mio bot è online 24/7 su Render!")
+
 
 bot.polling()
